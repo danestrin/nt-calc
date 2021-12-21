3d9bee706308f85dcb2ccf2248fd4f77a1264eed
Dan Estrin <danestrin4@gmail.com>
2021-12-21 13:38:37 -0800

    refactored main method to take cl args

--- NTCalculator/Program.cs
+++ NTCalculator/Program.cs
@@ -1,17 +1,27 @@
 ﻿using NTUtils;
 class Program {
     static void Main(string[] args) {
-        Console.WriteLine("Find prime numbers up to and including n.");
+        if (args == null || args.Length == 0) {
+            Console.WriteLine("[ERROR] No command entered.");
+            Environment.Exit(1);
+        }
 
-        Console.WriteLine("Enter n.");
-        int n = Convert.ToInt32(Console.ReadLine());
+        switch(args[0]) {
+            case "sieve":
+                if (args.Length != 2) {
+                    Console.WriteLine("[ERROR] Incorrect input format - correct format: sieve [integer]");
+                } else if (!args[1].All(Char.IsDigit)) {
+                    Console.WriteLine("[ERROR] Input for prime sieve must be a positive integer.");
+                } else {
+                    int n = Convert.ToInt32(args[1]);
+                    List<int> primes = PrimeUtils.PrimeSieve(n);
 
-        List<int> primes = PrimeUtils.PrimeSieve(n);
-
-        Console.WriteLine($"Prime numbers up to and including {n}:");
-        Console.WriteLine(String.Join(", ", primes));
-
-        Console.Write("Press any key to exit.");
-        Console.ReadKey();
+                    Console.WriteLine(String.Join(", ", primes));
+                }
+                break;
+            default:
+                Console.WriteLine("[ERROR] Unrecognized command.");
+                break;
+        }
     }
 }
