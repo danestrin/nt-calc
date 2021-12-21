12d6c79b8ffba9e2f4fa5156185f1b52ac807d16
Dan Estrin <danestrin4@gmail.com>
2021-12-20 21:54:25 -0800

    added starter code, prime sieve and driver code

---
+++ NTCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using NTUtils;
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Find prime numbers up to and including n.");
+
+        Console.WriteLine("Enter n.");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        List<int> primes = PrimeUtils.PrimeSieve(n);
+
+        Console.WriteLine($"Prime numbers up to and including {n}:");
+        Console.WriteLine(String.Join(", ", primes));
+
+        Console.Write("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
---
+++ NTUtils/PrimeUtils.cs
@@ -0,0 +1,29 @@
+﻿namespace NTUtils;
+public static class PrimeUtils
+{
+    public static List<int> PrimeSieve(int n) {
+        int[] sieve = new int[n + 1];
+        sieve[0] = 1;
+        sieve[1] = 1;
+
+        for (int i = 2; i*i <= n; i++) {
+            if (sieve[i] != 1) {
+                int j = i + i;
+                while (j <= n) {
+                    sieve[j] = 1;
+                    j += i;
+                }
+            }
+        }
+
+        var primes = new List<int>();
+
+        for (int i = 2; i < sieve.Length; i++) {
+            if (sieve[i] == 0) {
+                primes.Add(i);
+            }
+        }
+
+        return primes;
+    }
+}
